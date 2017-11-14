8d5e49bbfa2fbee216b9943366c4747620fd4676
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-14 13:18:40 +0000

    automatically backed up by learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,5 +1,16 @@
+require 'pry'
 class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    #binding.pry
+    Boat.select(:name).limit(5)
+  end
+
+  def self.dinghy
+    Boat.select(:name).where("length<20")
+  end
+
 end
