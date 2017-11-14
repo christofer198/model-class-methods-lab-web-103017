957d7ab7150ee7efb3ea8879193cb15e69d51579
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-14 13:46:44 +0000

    automatically backed up by learn

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -28,7 +28,7 @@ class Boat < ActiveRecord::Base
 
   def self.sailboats
     #binding.pry
-    Boat.joins(:classifications).where("classifications.name = 'Sailboat'")
+    Boat.join(:classifications).where("classifications.name = 'Sailboat'")
   end
 
   def
