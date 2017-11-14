e0432eaaaab369955cde4ad77a6ed68e5c0bf852
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-11-14 14:57:52 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,7 +2,7 @@ require 'pry'
 class Captain < ActiveRecord::Base
   has_many :boats
   def self.catamaran_operators
-    Boat.joins(:classifications).group('captain_id')
-    #binding.pry
+    Boat.joins(:classifications).group('captain_id').select('ca')
+    binding.pry
   end
 end
